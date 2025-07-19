be3221eb760fe8b491df4d79c2dd52b9efacfb68
Alan Szmyt <szmyty@gmail.com>
2025-07-18 22:25:11 -0400

    Add landing page and adjust blog routes

--- egohygiene.io/src/pages/rss.xml.js
+++ egohygiene.io/src/pages/rss.xml.js
@@ -8,9 +8,9 @@ export async function GET(context) {
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
+                items: posts.map((post) => ({
+                        ...post.data,
+                        link: `/blogs/${post.id}/`,
+                })),
 	});
 }
