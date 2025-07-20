370d5823390ba3e2bc501db0683c61c82ba5be73
Alan Szmyt <szmyty@gmail.com>
2025-07-19 23:59:22 -0400

    Update

--- egohygiene.io/astro.config.mjs
+++ egohygiene.io/astro.config.mjs
@@ -4,10 +4,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import path from 'node:path';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   site: 'https://egohygiene.io',
   base: '/',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
   publicDir: path.resolve('./public'),
   trailingSlash: "ignore",
 });
